011d4b11d7d43e47bb04fa57f48ce537f452dd2c
Sourav Banerjee <srvasn@gmail.com>
2016-12-30 03:31:57 +0530

    Removed constants into separate file

---
+++ constants.py
@@ -0,0 +1,18 @@
+# Declaring constants for use throughout the program
+
+STATE_AUTH = 'AUTH'
+STATE_CHAT = 'CHAT'
+SIGN_UP = 'SIGN_UP'
+LOGIN = 'REGISTER'
+EXIT = 'EXIT'
+EXIT_COMMAND = '.quit'  # This is what the user types when he/she wants to quit
+DB_URL = 'storage.db'
+PORT = 1236
+LOG_FILE_URL = 'chatserver.log'
+TEST_USER_FILE = 'sim_users.json'
+
+TEST_MESSAGES = ["Sample Message 1",
+                 "Sample Message 2",
+                 "Sample Message 3",
+                 "Sample Message 4",
+                 "Sample Message 5"]
